054fbea246fb80cdf6efe4672ee41d661e8466b4
Orlando MG <orlandomgr@gmail.com>
2026-04-15 23:25:26 -0700

    Additional performance fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://orlandomgr.github.io',
   output: 'static',
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
